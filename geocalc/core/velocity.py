20d5725779ec6c0253e875159a934a161f0208bc
Rawad Daher <rodi055@gmail.com>
2020-03-20 01:26:10 +0200

    Add velocity conversion: directional to cartesian

--- geocalc/core/velocity.py
+++ geocalc/core/velocity.py
@@ -7,3 +7,10 @@ def cartesian2directional(vx, vy, vz):
         raise ValueError('vx is 0. Cant divide by zero.')
     course = math.degrees(math.atan(vy / vx))
     return speed, course, vz
+
+
+def directional2cartesian(speed, course, vz):
+    course_in_rad = math.radians(course)
+    vx = speed * math.cos(course_in_rad)
+    vy = speed * math.sin(course_in_rad)
+    return vx, vy, vz
--- geocalc/tests/velocity_tests.py
+++ geocalc/tests/velocity_tests.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 
 from geocalc.core import velocity
@@ -9,6 +10,13 @@ class MyTestCase(unittest.TestCase):
         speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
         self.assertAlmostEqual(speed, 24.3, places=1)
         self.assertAlmostEqual(course, 30, places=0)
+        self.assertAlmostEqual(vz, 100, places=1)
+
+    def test_directional2cartesian(self):
+        speed, course, vz = 24.3, 30, 100
+        vx, vy, vz = velocity.directional2cartesian(speed, course, vz)
+        self.assertAlmostEqual(vx, 21, places=1)
+        self.assertAlmostEqual(vy, 12.2, places=0)
         self.assertAlmostEqual(vz, 100, places=1)
 
 

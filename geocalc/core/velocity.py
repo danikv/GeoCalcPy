52d6988f3882074c7f105432e64e149620abb6ab
Rawad Daher <rodi055@gmail.com>
2020-03-20 15:41:34 +0200

    velocity cartesian2directional: handle vx=0 and add tests

--- geocalc/core/velocity.py
+++ geocalc/core/velocity.py
@@ -3,9 +3,7 @@ import math
 
 def cartesian2directional(vx, vy, vz):
     speed = math.sqrt(vx * vx + vy * vy)
-    if vx == 0:
-        raise ValueError('vx is 0. Cant divide by zero.')
-    course = math.degrees(math.atan(vy / vx))
+    course = math.degrees(math.atan2(vy, vx))
     return speed, course, vz
 
 
--- geocalc/tests/velocity_tests.py
+++ geocalc/tests/velocity_tests.py
@@ -1,4 +1,3 @@
-import math
 import unittest
 
 from geocalc.core import velocity
@@ -11,6 +10,27 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(speed, 24.3, places=1)
         self.assertAlmostEqual(course, 30, places=0)
         self.assertAlmostEqual(vz, 100, places=1)
+
+    def test_cartesian2directional2(self):
+        vx, vy, vz = 14.6, -8.62, 100
+        speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
+        self.assertAlmostEqual(speed, 17, places=1)
+        self.assertAlmostEqual(course, -30.6, places=1)
+        self.assertAlmostEqual(vz, 100, places=1)
+
+    def test_cartesian2directional_vx_is_zero_vy_is_negative(self):
+        vx, vy, vz = 0, -8.62, 100
+        speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
+        self.assertEqual(speed, 8.62)
+        self.assertEqual(course, -90)
+        self.assertEqual(vz, 100)
+
+    def test_cartesian2directional_vx_is_zero_vy_is_positive(self):
+        vx, vy, vz = 0, 8.62, 100
+        speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
+        self.assertEqual(speed, 8.62)
+        self.assertEqual(course, 90)
+        self.assertEqual(vz, 100)
 
     def test_directional2cartesian(self):
         speed, course, vz = 24.3, 30, 100

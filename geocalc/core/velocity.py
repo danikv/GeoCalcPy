dc9f5024169612824581c0463ea85fc34114c081
Rawad Daher <rodi055@gmail.com>
2020-03-25 17:56:36 +0200

    Some place holders and name changing

--- geocalc/core/velocity.py
+++ geocalc/core/velocity.py
@@ -1,14 +1,28 @@
 import math
 
 
-def cartesian2directional(vx, vy, vz):
+def cartesian_to_directional(vx, vy, vz):
     speed = math.sqrt(vx * vx + vy * vy)
     course = math.degrees(math.atan2(vy, vx))
     return speed, course, vz
 
 
-def directional2cartesian(speed, course, vz):
+def directional_to_cartesian(speed, course, vz):
     course_in_rad = math.radians(course)
     vx = speed * math.cos(course_in_rad)
     vy = speed * math.sin(course_in_rad)
+    return vx, vy, vz
+
+
+def cartesian_to_polar(vx, vy, vz):
+    range_rate = 1
+    bearing_rate = 1
+    elevation_rate = 1
+    return range_rate, bearing_rate, elevation_rate
+
+
+def polar_to_ned_velocity(range, bearing, elevation, range_rate, bearing_rate, elevation_rate):
+    vx = 1
+    vy = 1
+    vz = 1
     return vx, vy, vz
--- geocalc/tests/velocity_tests.py
+++ geocalc/tests/velocity_tests.py
@@ -6,35 +6,35 @@ from geocalc.core import velocity
 class MyTestCase(unittest.TestCase):
     def test_cartesian2directional(self):
         vx, vy, vz = 21, 12.2, 100
-        speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
+        speed, course, vz = velocity.cartesian_to_directional(vx, vy, vz)
         self.assertAlmostEqual(speed, 24.3, places=1)
         self.assertAlmostEqual(course, 30, places=0)
         self.assertAlmostEqual(vz, 100, places=1)
 
     def test_cartesian2directional2(self):
         vx, vy, vz = 14.6, -8.62, 100
-        speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
+        speed, course, vz = velocity.cartesian_to_directional(vx, vy, vz)
         self.assertAlmostEqual(speed, 17, places=1)
         self.assertAlmostEqual(course, -30.6, places=1)
         self.assertAlmostEqual(vz, 100, places=1)
 
     def test_cartesian2directional_vx_is_zero_vy_is_negative(self):
         vx, vy, vz = 0, -8.62, 100
-        speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
+        speed, course, vz = velocity.cartesian_to_directional(vx, vy, vz)
         self.assertEqual(speed, 8.62)
         self.assertEqual(course, -90)
         self.assertEqual(vz, 100)
 
     def test_cartesian2directional_vx_is_zero_vy_is_positive(self):
         vx, vy, vz = 0, 8.62, 100
-        speed, course, vz = velocity.cartesian2directional(vx, vy, vz)
+        speed, course, vz = velocity.cartesian_to_directional(vx, vy, vz)
         self.assertEqual(speed, 8.62)
         self.assertEqual(course, 90)
         self.assertEqual(vz, 100)
 
     def test_directional2cartesian(self):
         speed, course, vz = 24.3, 30, 100
-        vx, vy, vz = velocity.directional2cartesian(speed, course, vz)
+        vx, vy, vz = velocity.directional_to_cartesian(speed, course, vz)
         self.assertAlmostEqual(vx, 21, places=1)
         self.assertAlmostEqual(vy, 12.2, places=0)
         self.assertAlmostEqual(vz, 100, places=1)
